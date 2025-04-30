a3e5d2875962262ed710e317c49a120dbbd58a21
Rodrigo050705 <135657705+Rodrigo050705@users.noreply.github.com>
2025-04-30 18:36:27 -0300

    Add files via upload

---
+++ Models/EstoqueModel.cs
@@ -0,0 +1,10 @@
+namespace EsbocoEstoque.Models
+{
+    public class EstoqueModel
+    {
+        public int EstoqueId { get; set; }
+        public int ProdutoId { get; set; }
+        public int QtnAtual { get; set; }
+
+    }
+}
