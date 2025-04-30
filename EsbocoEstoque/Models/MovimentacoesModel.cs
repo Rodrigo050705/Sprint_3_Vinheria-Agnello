aa28afd3cab0b2947cde8ddd61f872192d9f6e22
Rodrigo050705 <135657705+Rodrigo050705@users.noreply.github.com>
2025-04-30 18:38:05 -0300

    Add files via upload

---
+++ EsbocoEstoque/Models/MovimentacoesModel.cs
@@ -0,0 +1,12 @@
+namespace EsbocoEstoque.Models
+{
+    public class MovimentacoesModel
+    {
+        public int MovimentacaoId { get; set; }
+        public int ProdutoId { get; set; }
+        public string? TipoMovimentacao { get; set; }
+        public int QtnMovimentada { get; set; }
+        public bool Validacao { get; set; }
+
+    }
+}
